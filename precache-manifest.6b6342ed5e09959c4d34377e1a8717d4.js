7f4e1aa99cc70fc6f22587724271ce7d03b95c7a
Sahil Kalkal <s.k.kalkal456@gmail.com>
2020-07-01 02:55:44 +0530

    Updates

---
+++ precache-manifest.6b6342ed5e09959c4d34377e1a8717d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb5c7a33c8f85cce63a21221390d5c3",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "ee0a600e423316b0a379",
+    "url": "/chat/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "90ac7c6afe94aa42297e",
+    "url": "/chat/static/css/main.c158e4e3.chunk.css"
+  },
+  {
+    "revision": "ee0a600e423316b0a379",
+    "url": "/chat/static/js/2.4c673110.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat/static/js/2.4c673110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ac7c6afe94aa42297e",
+    "url": "/chat/static/js/main.7b53b919.chunk.js"
+  },
+  {
+    "revision": "8d86be0bcc4812848edd",
+    "url": "/chat/static/js/runtime-main.6b736942.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
